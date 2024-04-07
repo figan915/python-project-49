c1a87b23c84ca9946e02972bf39bbac05e62ff8b
Alex Susalev <figan915@gmail.com>
2024-04-07 12:40:46 +0300

    build package, update .gitignore

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import random
+import prompt
+#from cli import welcome_user
+
+def main():
+    print('Welcome to Brain Games')
+    user_name = prompt.string('May I have your name? ')
+    print(f'Hello, {user_name}!')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    for _ in range(3):
+        number = random.randint(1,100)
+        print(f'Question:  {number}')
+        answer = input()
+        print(f'Answer: {answer}')
+        if (number % 2 == 0 and answer.lower() == 'yes') or (number % 2 != 0 and answer.lower() == 'no'):
+            print('Correct')
+        elif (number % 2 == 0 and answer.lower() == 'no'):
+            print(f'no is wrong answer ;(. Correct answer was yes.\nLet\'s try again, {user_name}')
+            break
+        elif (number % 2 != 0 and answer.lower() == 'yes'):
+            print(f'yes is wrong answer ;(. Correct answer was no.\nLet\'s try again, {user_name}')
+            break
+        else:
+            print(f'is wrong answer ;(. Correct answer was yes or no.\nLet\'s try again, {user_name}')
+            break
+    return f'Congratulations,  {user_name}!'
+
+if __name__ == '__main__':
+    main()
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,11 +2,9 @@
 
 from cli import welcome_user
 
-
 def main():
     print('Welcome to Brain Games!')
     welcome_user()
-
 
 if __name__ == '__main__':
     main()
